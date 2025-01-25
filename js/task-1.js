31ab7459a1789f56b68c291443bad44a1bd40e82
letonzt <letonzt@gmail.com>
2025-01-25 12:47:28 +0100

    ADD HoMEwOrK JS 7

--- js/task-1.js
+++ js/task-1.js
@@ -1,28 +1,8 @@
-function isEnoughCapacity(products, containerSize) {
-    const values = Object.values(products);
-    let totalAmount = 0;
-    for (const value of values) {
-        totalAmount += value;
-    }
-    if (totalAmount <= containerSize) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-console.log(
-  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
-); // true
-
-console.log(
-  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
-); // false
-
-console.log(
-  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
-); // true
-
-console.log(
-  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
-); // false
+const allCategories = document.querySelectorAll('ul#categories>li.item');
+console.log("Number of categories:" + allCategories.length);
+allCategories.forEach(category => {
+    const title = category.querySelector('h2').textContent;
+    const elements = category.querySelectorAll('ul > li');
+    console.log("Category: " + title);
+    console.log("Elements: " + elements.length);
+});
--- js/task-3.js
+++ js/task-3.js
@@ -1,21 +1,5 @@
-const profile = {
-    username: 'Jacob',
-    playTime: 300,
-    changeUsername(newName) {
-        this.username = newName;
-    },
-    updatePlayTime(hours) {
-        this.playTime += hours;
-    },
-    getInfo() {
-        return `${this.username} has ${this.playTime} active hours!`;
-    }
-}
-
-console.log(profile.getInfo()); // "Jacob has 300 active hours!"
-
-profile.changeUsername("Marco");
-console.log(profile.getInfo()); // "Marco has 300 active hours!"
-
-profile.updatePlayTime(20);
-console.log(profile.getInfo()); // "Marco has 320 active hours!"
+const textInput = document.querySelector('input#name-input');
+const output = document.querySelector('span#name-output');
+textInput.addEventListener("input", (event) => {
+    const inputValue = event.currentTarget.value.trim();
+        output.textContent = inputValue.length>0 ? inputValue : "Anonymous"; });
